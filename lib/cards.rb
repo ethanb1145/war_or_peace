f348f72ae78149952396eecfac4d912534a167ca
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-05 13:07:49 -0600

    Create basic Card class, add attributes, initialize

--- lib/cards.rb
+++ lib/cards.rb
@@ -1,9 +1,12 @@
-class Deck
-    attr_reader :cards
+class Card
+    attr_reader :suit, :value, :rank
 
-    def initialize(cards)
-        @cards = cards
+    def initialize(suit,value,rank)
+        @suit = suit 
+        @value = value
+        @rank = rank
     end
 
-
 end
+
+
