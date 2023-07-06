262dc2114451f71f21948ee1e11ea6b823909dc7
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-05 19:59:25 -0600

    Add methods for the deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,15 +1,37 @@
+require './lib/cards'
 class Deck
     attr_reader :cards
+
 
     def initialize(cards)
         @cards = cards
     end
 
+
     def rank_of_card_at(index)
+        
+        @cards[index].rank    
+
     end
+
+
+    def high_ranking_cards
+
+        cards.select {|card| card.rank >= 11}
+
+    end
+
+    
+    def add_card(card)
+
+        @cards.push(card)
+
+    end
+
+
     
 
 
 
-
+    
 end
