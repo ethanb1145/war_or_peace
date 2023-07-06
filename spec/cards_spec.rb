92ad90542c42010ce6a0e8181ccb920cebb213b4
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-07-05 19:29:53 -0600

    Add test to check for readable attributes

--- spec/cards_spec.rb
+++ spec/cards_spec.rb
@@ -7,4 +7,13 @@ describe Card do
 
         expect(card1).to be_an_instance_of(Card)
     end
+
+
+    it "has readable attributes" do 
+        card1 = Card.new(:diamond, "Queen", 12)
+
+        expect(card1.suit).to eq(:diamond)
+        expect(card1.value).to eq("Queen")
+        expect(card1.rank).to eq(12)
+    end
 end
